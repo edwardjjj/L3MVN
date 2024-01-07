cd935b9a69136ccbaca0f5ac80abe6aa400b2ae8
edwardjjj <edward.jwjiang@gmail.com>
2024-01-07 17:59:31 +0900

    changed test.py

--- test.py
+++ test.py
@@ -2,6 +2,6 @@ import habitat
 from habitat.config.default import get_config
 
 config = get_config(config_path="objectnav.yaml")
-env = habitat.Env(config=config)
-obs = env.reset()
-print(obs.shape)
+env = habitat.RLEnv(config=config)
+obs, info = env.reset()
+print(obs)
