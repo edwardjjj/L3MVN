0cde73bbef1ed5de3701bb329001533d66962c21
edwardjjj <edward.jwjiang@gmail.com>
2024-01-07 17:23:23 +0900

    1

--- test.py
+++ test.py
@@ -1,6 +1,7 @@
+import habitat
 from habitat.config.default import get_config
 
-config = get_config(config_path="./envs/habitat/configs/tasks/objectnav_hm3d.yaml")
+config = get_config(config_paths=["./envs/habitat/configs/tasks/objectnav_hm3d.yaml"])
 
 
 env = habitat.Env(config=config)
